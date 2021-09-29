79bd2d9e2790c9f19ffb888c71ac537861d90280
Defunct <82825420+FamiliarNameMissing@users.noreply.github.com>
2021-09-28 20:41:07 -0500

    more notes

---
+++ programs/classes/Human.java
@@ -0,0 +1,13 @@
+public class Human {
+    
+    //instance variables
+    private boolean maskOn;
+    private int age;
+    
+    //class constructor
+    public Human(boolean askMask, int newAge) {
+        maskOn = askMask;
+        age = newAge;
+    }
+    
+}
